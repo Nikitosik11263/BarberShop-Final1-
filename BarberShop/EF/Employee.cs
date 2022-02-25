5952937cac15ae9ccff1f90a21bceb934949131d
Nikitosik11263 <80972952+Nikitosik11263@users.noreply.github.com>
2022-02-25 16:01:40 +0300

    ListView

--- BarberShop/EF/Employee.cs
+++ BarberShop/EF/Employee.cs
@@ -31,6 +31,7 @@ namespace BarberShop.EF
         public Nullable<int> SpecID { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
+        public byte[] Image { get; set; }
     
         public virtual Specialization Specialization { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
